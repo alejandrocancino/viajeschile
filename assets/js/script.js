3d7ec3461cfb83590d8adecc5ee237dfbe5041ab
“alejandrocancino” <alejandrocancino@MacBook-Pro-de-Alejandro.local>
2022-11-24 22:28:10 -0300

    finalizacion del proecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 
-    $(window).scroll(function(){
+   //NAVBAR TOP FIXED CON COLOR AL HACER SCROLL 
+
+   $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 300) {
           $(".scrolled").css("background" , "rgba(255, 255, 255, 0.87)");
@@ -13,3 +15,34 @@
             $(".brandtext").css("color" , "white");  	
         }
     })
+
+
+    //TOOLTIP
+
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
+
+    //ALERTA DE CORREO ENVIADO
+    $("#btnFormulario").click(function(){
+        alert("El correo fue enviado correctamente...");
+    });
+
+
+  //TOGGLE DE DESTACADOS
+
+    $(document).ready(function()
+    {
+    $("img").on( "click", function() {	 
+        $('img').toggle();
+        });
+    });
+
+    $(document).ready(function()
+    {
+    $("p").on( "click", function() {	 
+        $('img').toggle();
+        });
+    });
